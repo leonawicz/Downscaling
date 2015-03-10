4bc2c6a5261c567b205ded5ff956139d822081ff
Matt Leonawicz <matt.rstat@gmail.com>
2015-03-10 12:28:40 -0600

    Update code

--- code/ds_prism.R
+++ code/ds_prism.R
@@ -46,7 +46,40 @@ period.scenario <- if(scenario=="historical") c(scenario, "") else c("projected"
 models <- list.files(anomDir[i])
 models <- models[models %in% allowed.models]
 
-# @knitr function
+# @knitr func_trim2
+# Support function for method='akima'
+trim2 <- function(x,y=NULL,out="matrix",rc.ind=F,xy.ext=F){
+    if(!any(out==c("matrix","raster"))) stop("output must be a matrix or raster")
+	if(class(x)=="matrix") {
+		if(class(y)!="RasterLayer" & (out=="raster" | xy.ext==T)) stop("must input spatial information for raster or extent outputs")
+		if(out=="raster" | xy.ext==T) { cres <- 0.5*res(y); crs <- projection(y) }
+	}
+	if(class(x)=="RasterLayer") {
+		if(out=="raster" | xy.ext==T) { cres <- 0.5*res(x); crs <- projection(x);	y <- x }
+		x <- matrix(as.array(x),nrow=nrow(x),ncol=ncol(x))
+	}
+	if(class(x)!="matrix") { stop("x must be of class raster or matrix")
+	} else {
+		r.na <- c.na <- c()
+		for(i in 1:nrow(x)) r.na <- c(r.na, all(is.na(x[i,])))
+		for(i in 1:ncol(x)) c.na <- c(c.na, all(is.na(x[,i])))
+		r1 <- 1 + which(diff(which(r.na))>1)[1]; r2 <- nrow(x) -  which(diff(which(rev(r.na)))>1)[1]
+		c1 <- 1 + which(diff(which(c.na))>1)[1]; c2 <- ncol(x) - which(diff(which(rev(c.na)))>1)[1]
+		x <- x[r1:r2,c1:c2]
+		if(out=="raster" | xy.ext==T) {
+			xs <- xFromCol(y,col=c(c1,c2)) + c(-1,1)*cres[1]
+			ys <- yFromRow(y,row=c(r2,r1)) + c(-1,1)*cres[2]
+		}
+		if(out=="raster") x <- raster(x,xmn=xs[1],xmx=xs[2],ymn=ys[1],ymx=ys[2],crs=crs)
+		if(rc.ind==T & xy.ext==T) { x <- list(x=x,rc=c(r1,r2,c1,c2),xy=c(xs,ys))
+			} else if(rc.ind) { x <- list(x=x,rc=c(r1,r2,c1,c2))
+			} else if(xy.ext) { x <- list(x=x,xy=c(xs,ys))
+		}
+	}
+	return(x)
+}
+
+# @knitr func_proc
 # Processing function
 f <- function(j, i, par.by.month=FALSE, anomDir, outDir, b.clim.t, b.clim.p, method="akima"){
 	if(par.by.month) { mo <- j; jj <- j <- 1} else { mo <- 1:12; jj <- length(models) }
@@ -87,9 +120,11 @@ f <- function(j, i, par.by.month=FALSE, anomDir, outDir, b.clim.t, b.clim.p, met
 		b.anom.t <- rotate(crop(b.anom.t, ext))
 		b.anom.p <- rotate(crop(b.anom.p, ext))
 		projection(b.anom.t) <- projection(b.anom.p) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
+		gc()
+		print("Bricks loaded. Commence downscaling.")
 		
 		if(method=="akima"){
-		
+			require("akima")
 			proj4.out <- projection(r.clim.p)
 			nr <- nrow(r.clim.t)
 			nc <- ncol(r.clim.t)
@@ -111,7 +146,7 @@ f <- function(j, i, par.by.month=FALSE, anomDir, outDir, b.clim.t, b.clim.p, met
 					xy <-data.frame(xyFromCell(r.anom.p, 1:ncell(r.anom.p)))
 					coordinates(xy) <- c("x","y")
 					proj4string(xy)<- projection(r.anom.p)
-					xy <- coordinates(spTransform(xy, CRS=proj4.out))
+					xy <- coordinates(spTransform(xy, CRS=CRS(proj4.out)))
 					bb <- as.numeric(bbox(r.clim.t)*matrix(c(1.1,0.9,1.1,1.1),2,2))
 					e <- 120000 # extend all sides by 120 km
 					ind1 <- which(point.in.polygon(xy[,1], xy[,2], c(xmn - e, xmx + e, xmx + e, xmn - e), c(ymn - e, ymn + e, ymx + e, ymx - e)) > 0)
@@ -123,20 +158,18 @@ f <- function(j, i, par.by.month=FALSE, anomDir, outDir, b.clim.t, b.clim.p, met
 				z.t <- v.t[ind]; z.p <- v.p[ind]
 				spl.t <- interp(x, y, z.t, xo=x.out, yo=y.out, linear=T); spl.p <- interp(x, y, z.p, xo=x.out, yo=y.out, linear=T)
 				mat.t <- t(spl.t$z)[nr:1,]; mat.p <- t(spl.p$z)[nr:1,]
-				if(exists("rc.ind")) { mat.t <- mat.t[rc.ind[1]:rc.ind[2], rc.ind[3]:rc.ind[4]]; mat.p <- mat.t[rc.ind[1]:rc.ind[2], rc.ind[3]:rc.ind[4]] }
-				r.new.t <- raster(round(mat+mt.clim, 1), xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, crs=proj4.out)
-				r.new.p <- raster(round(mat*mp.clim), xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, crs=proj4.out)
+				if(exists("rc.ind")) { mat.t <- mat.t[rc.ind[1]:rc.ind[2], rc.ind[3]:rc.ind[4]]; mat.p <- mat.p[rc.ind[1]:rc.ind[2], rc.ind[3]:rc.ind[4]] }
+				r.new.t <- raster(round(mat.t + m.clim.t, 1), xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, crs=proj4.out)
+				r.new.p <- raster(round(mat.p * m.clim.p), xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, crs=proj4.out)
 				names(r.new.t) <- paste("tas_mean_C", grp, models[j], scenario, mos[k], yrs[h], sep="_")
 				names(r.new.p) <- paste("pr_total_mm", grp, models[j], scenario, mos[k], yrs[h], sep="_")
 				if(isCRU) { names(r.new.t) <- gsub("CRU_CRU_", "CRU_", names(r.new.t)); names(r.new.p) <- gsub("CRU_CRU_", "CRU_", names(r.new.p)) }
 				writeRaster(r.new.t, paste0(outDir.t, "/", names(r.new.t), ".tif"), datatype="FLT4S", options="COMPRESS=LZW", overwrite=T)
 				writeRaster(r.new.p, paste0(outDir.p, "/", names(r.new.p), ".tif"), datatype="FLT4S", options="COMPRESS=LZW", overwrite=T)
-				print(paste("######## Model", j, "of", jj, "| Month", k, "of 12 | Year", h, "of", nlayers(b.new.t), "########"))
+				print(paste("######## Model", j, "of", jj, "| Month", k, "of 12 | Year", h, "of", nlayers(b.anom.t), "########"))
 			}
 			
 		} else if(method=="gdal"){ # GDAL cannot process such a large chunk of data at once, use akima method for PRISM 2-km (or 771-m) downscaling
-			gc()
-			print("Bricks loaded. Commence downscaling.")
 			t.on <- Sys.time()
 			b.new.t <- projectRaster(b.anom.t, r.clim.t)
 			rm(b.anom.t)
@@ -207,6 +240,5 @@ return()
 # @knitr run
 # Run
 if(exists("month.index") && all(1:12 %in% month.index)){
-f(j=1, i=i, par.by.month=TRUE, anomDir=anomDir, outDir=outDir, b.clim.t=b.clim.t, b.clim.p=b.clim.p)
 	mclapply(month.index, f, i=i, par.by.month=TRUE, anomDir=anomDir, outDir=outDir, b.clim.t=b.clim.t, b.clim.p=b.clim.p, method="akima", mc.cores=length(month.index))
 } else mclapply(1:length(models), f, i=i, anomDir=anomDir, outDir=outDir, b.clim.t=b.clim.t, b.clim.p=b.clim.p, method="akima", mc.cores=min(length(models), 32))
